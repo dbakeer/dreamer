b35d0edea2f35c7240cc8659a1ea9910276f1f6b
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-11-24 13:27:18 -0500

    fully functioning

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,4 @@
 /////////////////////////////////
 /////// DREAM APP ANGULAR ///////
 /////////////////////////////////
-var app = angular.module('dreamApp', ['addCtrl', 'geolocation', 'gservice', 'ngRoute'])
-  .config(function($routeProvider){
-    $routeProvider.when('/join', {
-      controller: 'addCtrl',
-      templateUrl: './partials/addForm.html'
-    }).when('/find', {
-      templateUrl: './partials/queryForm.html'
-    }).otherwise({ redirectTo: '/join' });
-});
+var app = angular.module('dreamApp', ['addCtrl', 'geolocation', 'gservice']);
